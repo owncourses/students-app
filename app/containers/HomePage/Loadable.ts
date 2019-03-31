4988303bb4fd4e52f26a5924d2379c93998b0bbc
Bartlomiej Mikolajczuk <bartlomiej.mikolajczuk@outlook.com>
2019-03-31 12:22:23 +0200

    Home page tests

--- app/containers/HomePage/Loadable.ts
+++ app/containers/HomePage/Loadable.ts
@@ -1,7 +1,4 @@
-/**
- * Asynchronously loads the component for HomePage
- */
-import Loadable from "react-loadable";
+import * as Loadable from "react-loadable";
 
 import LoadingIndicator from "components/LoadingIndicator";
 
