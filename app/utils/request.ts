76fbd00ffa5442e0342cccadabe938a4c3db6d0e
Bartlomiej Mikolajczuk <bartlomiej.mikolajczuk@schibsted.com>
2019-07-07 09:36:18 +0200

    Fixed axios init

--- app/utils/request.ts
+++ app/utils/request.ts
@@ -2,7 +2,10 @@ import "whatwg-fetch";
 import config from "../../config/config";
 
 const axios = require("axios");
-axios.defaults.baseURL = `${config.server.url.replace(/\/$/, "")}/api`;
+
+if (typeof config.server.url === "string") {
+  axios.defaults.baseURL = `${config.server.url.replace(/\/$/, "")}/api`;
+}
 
 export default function request(options) {
   return axios(options);
