fb1f3feba2a0910204b67ce7381cd0ab66cae3b3
Bartlomiej Mikolajczuk <bartlomiej.mikolajczuk@outlook.com>
2020-05-23 09:12:36 +0200

    Removed mock data for blocked lesson
    Signed-off-by: Bartlomiej Mikolajczuk <bartlomiej.mikolajczuk@outlook.com>

--- app/containers/SingleCourse/reducer.ts
+++ app/containers/SingleCourse/reducer.ts
@@ -1,10 +1,6 @@
-import { fromJS } from "immutable";
+import {fromJS} from "immutable";
 
-import {
-  COURSE_ACTION,
-  COURSE_ACTION_SUCCESS,
-  COURSE_ACTION_ERROR
-} from "./constants";
+import {COURSE_ACTION, COURSE_ACTION_ERROR, COURSE_ACTION_SUCCESS} from "./constants";
 
 // The initial state of the App
 const initialState = fromJS({
@@ -18,9 +14,6 @@ function appReducer(state = initialState, action) {
     case COURSE_ACTION:
       return state.set("loading", true).set("error", false);
     case COURSE_ACTION_SUCCESS:
-      // TODO: Remove this mock when api is ready
-      action.course.purchase_url = "https://asystentciazy.pl/";
-      action.course.modules[0].lessons[0].blocked = true;
       return state.set("loading", false).set("course", action.course);
     case COURSE_ACTION_ERROR:
       return state.set("loading", false).set("error", action.error);
