6b3306add15708a371416ca2dd8e6b4124619558
Bartlomiej Mikolajczuk <bartlomiej.mikolajczuk@outlook.com>
2020-05-28 18:08:29 +0200

    Added animations

--- app/components/Notifications/Notifications.tsx
+++ app/components/Notifications/Notifications.tsx
@@ -9,6 +9,7 @@ import {
 } from "@material-ui/core";
 import Badge from "@material-ui/core/Badge";
 import Icon from "@material-ui/core/Icon";
+import SwipeableViews from "react-swipeable-views";
 import IconButton from "@material-ui/core/IconButton";
 import { useTranslation } from "react-i18next";
 import { NotificationsInterface } from "../../containers/Auth/interfaces";
@@ -26,6 +27,7 @@ const Notifications = ({
 }) => {
   const { t } = useTranslation();
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const [index, setIndex] = React.useState(0);
   const [selectedNotification, setSelectedNotification] = React.useState(null);
   const open = Boolean(anchorEl);
   const id = open ? "simple-popover" : undefined;
@@ -47,11 +49,13 @@ const Notifications = ({
         toggleNotification(id);
       }
       setSelectedNotification(foundNotification);
+      setIndex(1);
     }
   };
 
   const resetSelectedNotification = () => {
     setSelectedNotification(null);
+    setIndex(0);
   };
 
   if (!notifications) {
@@ -85,32 +89,14 @@ const Notifications = ({
           }}
         >
           <div className={"notifications-popover"}>
-            {selectedNotification && (
-              <IconButton
-                className={"navigate-back"}
-                color="inherit"
-                aria-label="Back"
-                size={"small"}
-                onClick={resetSelectedNotification}
-              >
-                <Icon>navigate_before</Icon>
-              </IconButton>
-            )}
-            <Typography variant={"subtitle1"} align={"center"}>
-              {selectedNotification
-                ? selectedNotification.title
-                : t("Notifications")}
-            </Typography>
-            <Box mt={1} mb={1}>
-              <Divider />
-            </Box>
-            {selectedNotification ? (
-              <Notification
-                key={selectedNotification.id}
-                notification={selectedNotification}
-              />
-            ) : (
-              <>
+            <SwipeableViews index={index} disabled enableMouseEvents>
+              <div className={`tab-${index}`}>
+                <Typography variant={"subtitle1"} align={"center"}>
+                  {t("Notifications")}
+                </Typography>
+                <Box mt={1} mb={1}>
+                  <Divider />
+                </Box>
                 {notifications.notifications.map(notification => (
                   <NotificationItem
                     key={notification.id}
@@ -118,8 +104,33 @@ const Notifications = ({
                     notification={notification}
                   />
                 ))}
-              </>
-            )}
+              </div>
+              <div className={`tab-${index}`}>
+                {selectedNotification && (
+                  <>
+                    <IconButton
+                      className={"navigate-back"}
+                      color="inherit"
+                      aria-label="Back"
+                      size={"small"}
+                      onClick={resetSelectedNotification}
+                    >
+                      <Icon>navigate_before</Icon>
+                    </IconButton>
+                    <Typography variant={"subtitle1"} align={"center"}>
+                      {selectedNotification.title}
+                    </Typography>
+                    <Box mt={1} mb={1}>
+                      <Divider />
+                    </Box>
+                    <Notification
+                      key={selectedNotification.id}
+                      notification={selectedNotification}
+                    />
+                  </>
+                )}
+              </div>
+            </SwipeableViews>
           </div>
         </Popover>
       </ClickAwayListener>
